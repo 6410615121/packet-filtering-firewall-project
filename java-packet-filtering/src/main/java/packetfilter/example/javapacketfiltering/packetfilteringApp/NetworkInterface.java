f2a90fe1bafeb8cb9ed617c49c94255941ea7bf3
suttha jongjaroen <6410615121@cn330>
2024-02-09 12:29:28 +0700

    add NetworkInterface.java

---
+++ java-packet-filtering/src/main/java/packetfilter/example/javapacketfiltering/packetfilteringApp/NetworkInterface.java
@@ -0,0 +1,11 @@
+package packetfilter.example.javapacketfiltering.packetfilteringapp;
+
+public class NetworkInterface {
+
+    public NetworkInterface() {
+    }
+
+    public void sendTo(String sourceIP, String destIP, String port) {
+        // implement later
+    }
+}
