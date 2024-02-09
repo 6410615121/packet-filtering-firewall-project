97a319efd51a0af756fd615925dc0307999c6ee2
suttha jongjaroen <6410615121@cn330>
2024-02-09 12:19:23 +0700

    add RuleManager.java

---
+++ java-packet-filtering/src/main/java/packetfilter/example/javapacketfiltering/packetfilteringApp/RuleManager.java
@@ -0,0 +1,24 @@
+package packetfilter.example.javapacketfiltering.packetfilteringapp;
+
+import java.util.ArrayList;
+import packetfilter.example.javapacketfiltering.packetfilteringapp.Rule;
+
+public class RuleManager {
+    private ArrayList<Rule> rules;
+
+    public RuleManager() {
+        rules = new ArrayList<Rule>();
+    }
+
+    public ArrayList<Rule> getRules() {
+        return rules;
+    }
+
+    public boolean addRule(Rule rule) {
+        return rules.add(rule);
+    }
+
+    public boolean removeRule(Rule rule) {
+        return rules.remove(rule);
+    }
+}
