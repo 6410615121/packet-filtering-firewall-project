bd74c40bc4e74cd9de487b244da5d69b0de64c09
suttha jongjaroen <6410615121@cn330>
2024-02-11 17:58:49 +0700

    update Device.java

--- java-packet-filtering/src/main/java/packetfilter/example/javapacketfiltering/packetfilteringApp/Device.java
+++ java-packet-filtering/src/main/java/packetfilter/example/javapacketfiltering/packetfilteringApp/Device.java
@@ -2,9 +2,11 @@ package packetfilter.example.javapacketfiltering.packetfilteringapp;
 
 public class Device {
     private String IPaddress;
+    private Firewall firewall;
 
     public Device(String IP) {
         IPaddress = IP;
+        firewall = new Firewall();
     }
 
     public String getIPaddress() {
@@ -13,5 +15,14 @@ public class Device {
 
     public void setIPaddress(String iPaddress) {
         IPaddress = iPaddress;
+    }
+
+    public Firewall getFirewall() {
+        return firewall;
+    }
+
+    public boolean sendTo(String destIP, String port) {
+        boolean isAllow = firewall.isAllowPacket(IPaddress, destIP, port);
+        return isAllow;
     }
 }
