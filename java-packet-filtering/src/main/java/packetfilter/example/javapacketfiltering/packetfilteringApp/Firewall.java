0606480c3a223d430f75f58a28d672bb81c6d02b
suttha jongjaroen <6410615121@cn330>
2024-02-11 17:52:26 +0700

    update Firewall

--- java-packet-filtering/src/main/java/packetfilter/example/javapacketfiltering/packetfilteringApp/Firewall.java
+++ java-packet-filtering/src/main/java/packetfilter/example/javapacketfiltering/packetfilteringApp/Firewall.java
@@ -1,8 +1,16 @@
 package packetfilter.example.javapacketfiltering.packetfilteringapp;
 
+import java.util.ArrayList;
+
 public class Firewall {
     private RuleManager ruleManager;
+
     private boolean isActive;
+
+    public Firewall() {
+        isActive = true;
+        ruleManager = new RuleManager();
+    }
 
     public void enable() {
         isActive = true;
@@ -16,10 +24,31 @@ public class Firewall {
         return isActive;
     }
 
-    public boolean isAllowPacket(String sourceIP, String destIP, String port) {
+    public RuleManager getRuleManager() {
+        return ruleManager;
+    }
 
-        // implement later
-        return true;
+    public boolean isAllowPacket(String sourceIP, String destIP, String port) {
+        ArrayList<Rule> rules = ruleManager.getRules();
+
+        for (int i = 0; i < rules.size(); i++) {
+            Rule rule = rules.get(i);
+
+            String ruleSourceIP = rule.getSourceIP();
+            String ruleDestIP = rule.getDestIP();
+            String rulePort = rule.getPort();
+
+            // equalling -1 means any
+            boolean cond1 = (sourceIP == ruleSourceIP) || (ruleSourceIP.equals("-1")); // sourceIP
+            boolean cond2 = (destIP == rule.getDestIP()) || (ruleDestIP.equals("-1")); // destIP
+            boolean cond3 = (port == rulePort) || (rulePort.equals("-1")); // port
+
+            if (cond1 && cond2 && cond3) {
+                return rule.isAllow();
+            }
+
+        }
+        return false;
     }
 
 }
